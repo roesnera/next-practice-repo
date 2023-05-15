6937b2145a9a26e119cdf64d3a3e2737f55ab249
roesnera <adam.roesner@generation.org>
2023-05-15 11:00:01 -0400

    configures articles page with next routing, ssr

--- app/articles/page.jsx
+++ app/articles/page.jsx
@@ -1,4 +1,28 @@
-export default function page() {
+import Article from "../Article";
+import "./Articles.css";
 
-    return <div>Hello from the articles page!👋</div>
+export default async function page() {
+
+
+    const fetchArticles = async () => {
+      const fetchedArticles = await fetch("http://localhost:3001/articles");
+      // console.log(fetchedArticles);
+      const parsedArticles = await fetchedArticles.json();
+      // console.log(parsedArticles);
+      return parsedArticles;
+    }
+
+    const articles = await fetchArticles();
+
+
+    const renderArticles = () => {
+      const renderedArray = articles.map((art, ind) => {
+        return <Article key={ind} text={art.text} author={art.author} />
+      });
+      return renderedArray;
+    }
+
+    const renderedArticles = await renderArticles();
+
+    return <div className='articles-page'>{renderedArticles}</div>
 }
